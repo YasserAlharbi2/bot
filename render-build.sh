ce8b2b2aedc663449b4e1df148f368eb179b2dc4
YasserAlharbi2 <110176361+YasserAlharbi2@users.noreply.github.com>
2025-04-24 00:01:36 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,8 @@
 
 # تثبيت Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt update
 apt install -y ./google-chrome-stable_current_amd64.deb
 
