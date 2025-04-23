3320f9d1041aae1981e3a1a36e191f661b6147ef
YasserAlharbi2 <110176361+YasserAlharbi2@users.noreply.github.com>
2025-04-24 00:30:07 +0300

    Update import library bot_runner.py

--- bot_runner.py
+++ bot_runner.py
@@ -1,5 +1,8 @@
 import time
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
