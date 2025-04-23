39edb9aa245c2c128f5ead066fce655d272226c3
YasserAlharbi2 <110176361+YasserAlharbi2@users.noreply.github.com>
2025-04-24 00:11:07 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
+# تثبيت Chromium
+apt-get update
+apt-get install -y chromium
 
-# تثبيت Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt update
-apt install -y ./google-chrome-stable_current_amd64.deb
-
-# تأكد من وجود المسار:
-which google-chrome
-chmod +x render-build.sh
+# تستكمل بعدها تثبيت باقي المتطلبات
+pip install -r requirements.txt
