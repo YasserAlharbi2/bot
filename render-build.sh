885087d2e2962613406f6ecacc805d38ccac28e8
YasserAlharbi2 <110176361+YasserAlharbi2@users.noreply.github.com>
2025-04-23 23:50:51 +0300

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# تثبيت Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt update
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# تأكد من وجود المسار:
+which google-chrome
+chmod +x render-build.sh
